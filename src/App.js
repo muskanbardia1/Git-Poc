1dbe862e49d4829ddf232e9056ed26ce37ddd39d
muskan.bardia <muskan.bardia@exaltaretech.com>
2021-08-17 12:56:54 +0530

    newbranch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>My POC</h2>
     </div>
   );
 }
